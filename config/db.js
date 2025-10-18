8b9fd313ae8ed0e8d73e3fbd0a6d2f607386171e
hayyantahirr <hayyantahir7@gmail.com>
2025-10-18 19:09:00 +0500

    db connected

---
+++ config/db.js
@@ -0,0 +1,6 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+mongoose.connect(process.env.MONGO_DB_CONNECTION_URI);
+export default mongoose;
---
+++ index.mjs
@@ -0,0 +1,17 @@
+import express from "express";
+// import router from "./routes/index.js";
+import db from "./config/db.js";
+const app = express();
+
+app.listen(4000, () => {
+  console.log("Listening at 4000");
+  db.connection.once("open", () => {
+    console.log("db connect hogya lovely ");
+  });
+});
+
+
+//http://localhost:4000/teachers
+//POST
+app.use(express.json()); //backend ko bata rhay hai frontend ka data JSON format mai arha hai
+// app.use("/", router);
